726bd77bddbb4f1dd3a296e49301b83c26b5cce6
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-10-01 20:25:07 +0300

    more hooks for form logic

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,10 +4,12 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const [inputState, seInputState] = useState({ title: '', amount: '' })
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredAmount, setEnteredAmount] = useState('')
+
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({title: enteredTitle, amount: enteredAmount})
   };
 
   return (
@@ -16,30 +18,19 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState.title}
+            <input type="text" id="title"
+              value={enteredTitle}
               onChange={event => {
-                const newTitle = event.target.value;
-                seInputState((prevInputState) => (
-                  {
-                    title: newTitle,
-                    amount: prevInputState.amount
-                  }))
-              }
-              }
-
+                setEnteredTitle(event.target.value)
+              }}
             />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState.amount}
+            <input type="text" id="amount" 
+              value={enteredAmount}
               onChange={event => {
-                const newAmount = event.target.value
-                seInputState((prevInputState) => (
-                  {
-                    amount: newAmount,
-                    title: prevInputState.title
-                  })
-                )
+                setEnteredAmount(event.target.value)
               }} />
           </div>
           <div className="ingredient-form__actions">
